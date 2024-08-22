bbec113088299c779750cb6591c3b6313177ac3c
Milena Anjos Sigoli <milena.anjos@aluno.ifsp.edu.br>
2024-08-21 22:06:12 -0300

    scaffold do controller Piadas

---
+++ Models/Piada.cs
@@ -0,0 +1,13 @@
+﻿namespace Piadas.Models;
+
+public class Piada
+{
+    public int Id { get; set; }
+    public string Pergunta { get; set; }
+    public string Resposta { get; set; }
+
+    public Piada()
+    {
+        
+    }
+}
